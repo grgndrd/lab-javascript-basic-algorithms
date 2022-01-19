d5c3e276af07adcbaffb06faf75f747d568589b4
grgndrd <greeg.andradee@gmail.com>
2022-01-19 21:08:47 +0000

    solved 3.3 using localeCompare

--- js/index.js
+++ js/index.js
@@ -42,11 +42,10 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 console.log(reversedDriver);
 
 // 3.3
-if (hacker1 > hacker2) {
-  console.log("The driver's name goes first.");
-} else if (hacker1 < hacker2) {
-  console.log("Yo, the navigator goes first definitely.");
+if (driver1.localeCompare(driver2) === 1) {
+  console.log(`The driver's name goes first.`);
+} else if (driver1.localeCompare(driver2) === 0) {
+  console.log(`Yo, the navigator goes first definitely.`);
 } else {
-  console.log("What?! You both have the same name?");
+  console.log(`What?! You both have the same name?`);
 }
-
